7b1e4eb64f89f8f497077e413d7a316e970fd884
Michael Montaque <montaque.developer@gmail.com>
2023-12-10 01:08:19 -0800

    Fixed duplication in call intent node

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const storage = require("node-persist");
 
-let didInit = false;
 //you must first call storage.init
 async function getDatabase(cb) {
-  if (!didInit) {
+  if (!getDatabase.didRun) {
+    getDatabase.didRun = true;
     await storage.init();
+    await storage.clear();
   }
   cb(storage);
 }
